fcf8595dbdae2e0ab1de3b185bb88a345a1324ff
unknown <formacio@PC-1062618.formacio.local>
2021-07-20 13:00:11 +0200

    Work in progress

--- callback_hell.js
+++ callback_hell.js
@@ -11,13 +11,34 @@ str
 .reverse()
 .join("");
 
+async function getFilePaths(){
+   let files = await fs.promises.readdir(inbox)
+   return files
+}
 
-fs.promises.readdir(inbox)
-.then(x=>{x.forEach(file=>
-  fs.promises.readFile(join(inbox,file), 'utf8')
-  .then(text=>
-    fs.promises.writeFile(join(outbox,file), reverseText(text))
-    )
-    )})
+async function printFiles () {
+  const files = await getFilePaths();
+
+  for (const file of files) {
+    const contents = await fs.promises.readFile(join(inbox,file), 'utf8');
+    console.log(contents)
+    return contents;
+  }
+}
+
+async function reverse(){
+  const text = await printFiles()
+  fs.promises.writeFile(join(outbox,file), reverseText(text))
+}
+
+printFiles()
+
+// fs.promises.readdir(inbox)
+// .then(x=>{x.forEach(file=>
+//   fs.promises.readFile(join(inbox,file), 'utf8')
+//   .then(text=>
+//     fs.promises.writeFile(join(outbox,file), reverseText(text))
+//     )
+//     )})
     
     
